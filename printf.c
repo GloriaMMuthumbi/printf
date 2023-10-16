52d9389dd8623ee5a31486270fcc11cd6c47b7d5
Paul Ogolla <paulotieno2@gmail.com>
2023-10-16 15:02:57 +0300

    start

--- printf.c
+++ printf.c
@@ -6,6 +6,20 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
+  va_list args;
+  int i;
+
+  va_start(args, )
+  i = 0;
+
+  while (format[i] < '\0')
+  {
+    i++;
+  }
+
+  va_start(args, i)
+  
+  va_end(args);
 
 
 	return (count);
