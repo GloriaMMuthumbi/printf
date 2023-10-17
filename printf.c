2aee80b1a0183706480504efd0d8c06859cfbf2b
Paul Ogolla <paulotieno2@gmail.com>
2023-10-17 21:53:49 +0300

    undo

--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include "stddef.h"
 /**
  * _printf - prints number of characters
  * @format: the formatted string
@@ -16,8 +15,6 @@ int _printf(const char *format, ...)
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	if (format == NULL)
 		return (-1);
 	while (*format)
 	{
