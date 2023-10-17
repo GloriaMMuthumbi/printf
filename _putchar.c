0565f930221e7cead2bb5e258735b61865508079
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-10-17 00:02:19 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,11 @@
 #include <unistd.h>
-
+/**
+ * _putchar - prints a character
+ * @c: character to be printed
+ *
+ * Return: returns success
+ * */
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
--- printf.c
+++ printf.c
@@ -1,26 +1,65 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdarg.h>
 /**
+ * _printf - prints number of characters
+ * @format: the formatted string
  *
+ * Return: returns the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	int count = 0;
-  va_list args;
-  int i;
+	char *str;
+	va_list args;
 
-  va_start(args, )
-  i = 0;
+	if (format == NULL)
+		return (-1);
 
-  while (format[i] < '\0')
-  {
-    i++;
-  }
-
-  va_start(args, i)
-  
-  va_end(args);
-
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			if (*format == 'r')
+				count += _putchar('r');
+			else
+			{
+				switch (*format)
+				{
+					case 'c':
+						count += _putchar(va_arg(args, int));
+						break;
+					case 's':
+						{
+							str = va_arg(args, char *);
+							if (str == NULL)
+							str = "(null)";
+							while (*str)
+							{
+								count += _putchar(*str);
+								str++;
+							}
+						}
+						break;
+					case '%':
+						count += _putchar('%');
+						break;
+					default:
+						count += _putchar('%');
+						count += _putchar(*format);
+						break;
+				}
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
 
 	return (count);
 }
