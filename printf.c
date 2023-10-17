90fc1c8f5bb221ff0d0356309923bc7a9b3a8386
Paul Ogolla <paulotieno2@gmail.com>
2023-10-17 12:25:32 +0300

    fail case

--- printf.c
+++ printf.c
@@ -14,13 +14,13 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (*format)
 	{
-		if (!format || (format[0] == '%' && !format[1]))
-			return (-1);
-		if (format[0] == '%' && format[1] == ' ' && !format[2])
-			return (-1);
-		if (*format == '%' && *(format + 1))
+		if (*format == '%')
 		{
 			format++;
 			switch (*format)
