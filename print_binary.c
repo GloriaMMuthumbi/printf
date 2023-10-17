6e77b48dfe9077e0615cca2e75208f25fd4587d7
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-10-17 08:55:50 -0700

    task 2

--- print_binary.c
+++ print_binary.c
@@ -1,14 +1,30 @@
 #include "main.h"
 /**
- *
+ * print_binary - prints unsigned integer in its binary
+ * form
+ * @n: unsigned integer to be printed
+ * Return: returns number of characters to be printed
  */
 int print_binary(unsigned int n)
 {
+	int count = 0;
+	char binary[32];
+
 	if (n == 0)
 		return (_putchar('0'));
 
-	if (n / 2)
-		print_binary(n / 2);
+	while (n > 0)
+	{
+		binary[count] = (n & 1) + '0';
+		n >>= 1;
+		count++;
+	}
 
-	return (_putchar('0' + (n % 2)));
+	for (count--; count >= 0; count--)
+	{
+		_putchar(binary[count]);
+		count++;
+	}
+
+	return (count);
 }
