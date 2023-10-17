0ca54942ba34ac31943a1b2c4048c294526bed9e
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-10-17 01:26:34 -0700

    printf task 0

--- printf.c
+++ printf.c
@@ -9,57 +9,42 @@
  */
 int _printf(const char *format, ...)
 {
-        int count = 0;
-        char *str;
-        va_list args;
+	int count = 0;
+	char *str;
+	va_list args;
 
-        if (format == NULL)
-                return (-1);
-
-        va_start(args, format);
-        while (*format != '\0')
-        {
-                if (*format == '%' && *(format + 1))
-                {
-                        format++;
-                        if (*format == 'r')
-                                count += _putchar('r');
-                        else
-                        {
-                                switch (*format)
-                                {
-                                        case 'c':
-                                                count += _putchar(va_arg(args, int));
-                                                break;
-                                        case 's':
-                                                {
-                                                        str = va_arg(args, char *);
-                                                        if (str == NULL)
-                                                        str = "(null)";
-                                                        while (*str)
-                                                        {
-                                                                count += _putchar(*str);
-                                                                str++;
-                                                        }
-                                                }
-                                                break;
-                                        case '%':
-                                                count += _putchar('%');
-                                                break;
-                                        default:
-                                                count += _putchar('%');
-                                                count += _putchar(*format);
-                                                break;
-                                }
-                        }
-                }
-                else
-                {
-                        count += _putchar(*format);
-                }
-                format++;
-        }
-        va_end(args);
-
-        return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 's':
+				{
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+						count += _putchar(*str++);
+				}
+					break;
+				default:
+					count += _putchar('%') + _putchar(*format);
+					break;
+			}
+		}
+		else
+			count += _putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
