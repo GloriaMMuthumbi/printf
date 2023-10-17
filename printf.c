cacc057ccbdeecd6fe6f1fe0bed24f5369c5593d
Paul Ogolla <paulotieno2@gmail.com>
2023-10-17 09:53:35 +0300

    attempt

--- printf.c
+++ printf.c
@@ -1,26 +1,42 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
- *
- */
+ * _printf - printing function
+ * @format: sing
+ * Return: (0)
+*/
+
 int _printf(const char *format, ...)
 {
-	int count = 0;
-  va_list args;
-  int i;
+	int counter = 0;
+	int i;
+	char *s;
+	char c;
+	va_list args;
 
-  va_start(args, )
-  i = 0;
-
-  while (format[i] < '\0')
-  {
-    i++;
-  }
-
-  va_start(args, i)
-  
-  va_end(args);
-
-
-	return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		switch (*format++)
+		{
+			case 's':
+				s = va_arg(args, char *);
+				_putchar(s);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				_putchar(i);
+				break;
+			case 'c':
+				c = (char) va_arg(args, int);
+				_putchar(c);
+				break;
+		}
+		i++;
+		counter += 1;
+	}
+	va_end(args);
+	return (counter);
 }
